0b190177eb1f9b638dab81500a0cc7a5d2d2277c
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-08-29 22:57:38 -0500

    cmaj created

---
+++ go/song/majorKeys.go
@@ -0,0 +1,15 @@
+package song
+
+var cMaj = Key{
+	Root: "C",
+	Mode: "Major",
+	Notes: []Note{
+		{Note: "C", Degree: "I"},
+		{Note: "D", Degree: "ii"},
+		{Note: "E", Degree: "iii"},
+		{Note: "F", Degree: "IV"},
+		{Note: "G", Degree: "V"},
+		{Note: "A", Degree: "vi"},
+		{Note: "B", Degree: "vii°"},
+	},
+}
